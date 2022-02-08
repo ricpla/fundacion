6fb6d13f8e9bd65a983a4800fc0770dbf862ef59
ricpla <ricpla17@gmail.com>
2022-02-08 17:49:08 -0400

    enter

---
+++ index.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.scroll1');
+ScrollReveal().reveal('.scroll2', { delay: 500 });
+ScrollReveal().reveal('.scroll3', { delay: 600 });
+ScrollReveal().reveal('.scroll4', { delay: 700 });
